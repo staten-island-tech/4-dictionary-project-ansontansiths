9e4b87f21cc623518052772e2a01387d9b6f540c
ansontansiths <ansont5@nycstudents.net>
2026-02-27 10:16:43 -0500

    day 19
    day 19

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+def dictionary():
+    print("welcome to the shop dictionary what would you like to search for?")
+    
+    def search():
+        search = input("enter item name:")
+
+        if search == "garlic bread":
+            print("garlic bread is in the hot food area would you like to purchase this item?")
+        elif search == "milk":
+            print("milk is in the refrigeration area would you like to purchase this item?")
+        elif search == "eggs":
+            print("eggs are in the refrigeration area would you like to purchase this item?")
+        elif search == else:
+            print("sorry we do not have that item in stock would you like to search for something else?")
+            search()
